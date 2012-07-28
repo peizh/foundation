2deb4e99ef8df3e10a7f2db28137f3d6d105b30a
Demon Deng <dxssx.dxssx@gmail.com>
2012-07-28 22:41:33 +0800

    a

--- String8.cpp
+++ String8.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include <stdio.h>
 #include "AndroidConfig.h"
 #include "String8.h"
 
---
+++ foundationTests/foundationTests.cpp
@@ -0,0 +1,56 @@
+//
+//  foundationTests.m
+//  foundationTests
+//
+//  Created by dx on 12-7-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include "RefBase.h"
+#include "Mutex.h"
+#include "Condition.h"
+#include "Log.h"
+
+#include "ALooper.h"
+#include "AMessage.h"
+#include "ABuffer.h"
+#include "ABitReader.h"
+#include "ALooper.h"
+#include "AHandler.h"
+#include "AString.h"
+
+using namespace android;
+
+class MyHandler: public AHandler {
+  protected:
+    virtual void onMessageReceived(const sp<AMessage> &msg) {
+      switch(msg->what()) {
+        case 'test': 
+          {
+            Mutex::Autolock autoLock(mLock);
+            ALOGI("test");
+            mCond.signal();
+          }
+      }
+    };
+  private:
+    Mutex mLock;
+    Condition mCond;
+};
+
+int main() {
+  sp<ALooper> looper = new ALooper();
+  sp<ABuffer> buffer = new ABuffer(256);
+  AString string = "aaaa";
+  sp<AMessage> message = new AMessage('test');
+  message->setInt32("aaa", 1);
+  message->setBuffer("bbb", buffer);
+  message->setString("ccc", string.c_str());
+  sp<AHandler> handler = new MyHandler();
+  looper->registerHandler(handler);
+  looper->start();
+  message->post();
+
+
+  return 0;
+}
